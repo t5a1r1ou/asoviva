e8da7e9f846f6be57c9f4dd0053a6fb5a5ca9228
Shutto <baw.keepon69@gmail.com>
2019-08-24 02:05:20 +0900

    invalidation turbolinks

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,29 +1,29 @@
-document.addEventListener('turbolinks:load', function() {
+document.addEventListener('DOMContentLoaded', function() {
 
   var sidenav = document.querySelectorAll('.sidenav');
-   var sidenav_instances = M.Sidenav.init(sidenav);
+  var sidenav_instances = M.Sidenav.init(sidenav);
 
-   var post_modal = document.querySelectorAll('.modal');
-    var postmodal_instances = M.Modal.init(post_modal);
+  var post_modal = document.querySelectorAll('.modal');
+  var postmodal_instances = M.Modal.init(post_modal);
 
-    var today = new Date
-    var datepicker = document.querySelectorAll('.datepicker');
-    var datepicker_instances = M.Datepicker.init(datepicker,
+  var today = new Date
+  var datepicker = document.querySelectorAll('.datepicker');
+  var datepicker_instances = M.Datepicker.init(datepicker,
     {
       autoClose: true,
       format: "yyyy/mm/dd",
       minDate: today,
       container: document.querySelector('#calender'),
       i18n: {
-            cancel: 'キャンセル',
-            clear: 'クリア',
-            done: '決定',
-            months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
-            monthsShort:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
-            weekdays:['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
-            weekdaysShort:['日','月','火','水','木','金','土'],
-            weekdaysAbbrev:['日','月','火','水','木','金','土']
-        },
+        cancel: 'キャンセル',
+        clear: 'クリア',
+        done: '決定',
+        months:["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"],
+        monthsShort:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+        weekdays:['日曜日','月曜日','火曜日','水曜日','木曜日','金曜日','土曜日'],
+        weekdaysShort:['日','月','火','水','木','金','土'],
+        weekdaysAbbrev:['日','月','火','水','木','金','土']
+      },
       showClearBtn: true
     });
 
@@ -37,4 +37,4 @@ document.addEventListener('turbolinks:load', function() {
     var character_counter_textarea = document.querySelector('#area_description');
 
     M.CharacterCounter.init(character_counter_textarea);
- });
+  });
