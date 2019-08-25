96f60155f71aff1faf4047b74462ee48bf430358
Shutto <baw.keepon69@gmail.com>
2019-08-25 16:20:47 +0900

    done error handling for crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    if post.save!
-      redirect_to posts_url, notice: "あなたの「#{post.name}に行きたい！」を登録しました！"
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_url, notice: "#{@post.name}に行く予定を登録しました！"
     else
+      @posts = Post.page(params[:page]).per(9)
+      flash.now[:alert] = "エリアが初期化されています。ご注意ください"
       render :index
     end
   end
@@ -22,15 +24,19 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update!(post_params)
-    redirect_to posts_url, notice: "#{post.name}に行く予定を更新しました！"
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to posts_url, notice: "#{@post.name}に行く予定を更新しました！"
+    else
+      flash.now[:alert] = "エリアが初期化されています。ご注意ください"
+      render :edit
+    end
   end
 
   def destroy
     post = Post.find(params[:id])
     post.destroy
-    redirect_to posts_url, notice: "#{post.name}にいく予定を削除しました"
+    redirect_to posts_url, notice: "#{post.name}に行く予定を削除しました"
   end
 
   private
