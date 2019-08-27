198032493eca438a0e68d701c9f7417317224341
Shutto <baw.keepon69@gmail.com>
2019-08-27 23:42:04 +0900

    create devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   devise_for :users, :controllers => {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
-  root "static_pages#home"
   resources :posts, except: [:new]
 end
