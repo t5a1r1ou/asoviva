025d61ea42f8706ae6e88cf45769b61f1c257d05
Shutto <baw.keepon69@gmail.com>
2019-09-05 21:12:00 +0900

    enlarge twitter images

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -34,7 +34,7 @@ Devise.setup do |config|
 
   config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'], name: :google
 
-  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], :image_size => 'original'
+  config.omniauth :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], :image_size => 'large'
 
   # ==> Configuration for any authentication mechanism
   # Configure which keys are used when authenticating a user. The default is
